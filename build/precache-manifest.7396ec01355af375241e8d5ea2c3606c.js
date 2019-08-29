646969014f9360a2c1c299a808f593c46f11d132
rafaelmonroy <rafaelmonroy1990@gmail.com>
2019-08-28 22:09:40 -0700

    title update

---
+++ build/precache-manifest.7396ec01355af375241e8d5ea2c3606c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5917fe4087971059e82ae40ba9efd9e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6786c90119160cd38e6",
+    "url": "/static/css/main.9512833a.chunk.css"
+  },
+  {
+    "revision": "491731e795a499fe6889",
+    "url": "/static/js/2.c1964a71.chunk.js"
+  },
+  {
+    "revision": "b6786c90119160cd38e6",
+    "url": "/static/js/main.9eacea24.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
