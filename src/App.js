043b5ec86663eca9f97fdaea07f1b068414b867f
Rafael Monroy <rafaelmonroy1990@gmail.com>
2020-04-02 19:37:49 -0700

    rm tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import QuoteBox from "./QuoteBox";
-import ReactFCCtest from "react-fcctest";
 
 function App() {
   return (
     <main>
-      <ReactFCCtest />
       <QuoteBox />
     </main>
   );
